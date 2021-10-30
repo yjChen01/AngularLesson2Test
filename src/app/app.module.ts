48ced6773d8e720429692235258ceb943e1e7796
yjChen01 <87367152+yjChen01@users.noreply.github.com>
2021-10-30 11:25:22 +0800

    建立專案並切版

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './article/article.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticleComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
